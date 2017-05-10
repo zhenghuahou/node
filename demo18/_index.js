ed59998c2b4d1daf8d021f4b2861921e3d05c829
侯正华 <houzhenghua.sh@superjia.com>
2017-05-10 14:27:28 +0800

    add

--- demo18/_index.js
+++ demo18/_index.js
@@ -2,20 +2,33 @@
 //[num]:num为执行顺序
     console.log(+new Date,'[1]:golb1',process);//1
     setTimeout(function() {
-        console.log('[8]:timeout1'); //8
+        setTimeout(function(){
+            console.warn('[after 15 && before 16] timeout3',+new Date);
+        },0)
+        console.log('[8]:timeout1',+new Date); //8
         process.nextTick(function() {
-            console.log('[12]:timeout1_nextTick');//12
+            console.log('[12-1]:timeout1_nextTick',+new Date);//12-1
         })
+        process.nextTick(function() {
+            console.log('[12-2]:timeout1_nextTick',+new Date);//12-2
+        })
+       
         new Promise(function(resolve) {
-            console.log('[9]:timeout1_promise');//9
+            console.log('[9]:timeout1_promise',+new Date);//9
             resolve();
         }).then(function() {
-            console.log('[14]:timeout1_then') //14
+            console.log('[14]:timeout1_then',+new Date) //14
         })
-    })
+        process.nextTick(function() {
+            console.log('[12-3]:timeout1_nextTick',+new Date);//12-3
+        })
+        process.nextTick(function() {
+            console.log('[12-4]:timeout1_nextTick',+new Date);//12-4
+        })
+    },0)
      
     setImmediate(function() {
-        console.log('[16]:immediate1');//16
+        console.log('[16]:immediate1',+new Date);//16
         process.nextTick(function() {
             console.log('[20]:immediate1_nextTick');//20
         })
@@ -37,19 +50,7 @@
         console.log('[6]:glob1_then') //6
     })
      
-    setTimeout(function() {
-        console.log('[10]:timeout2');//10
-        process.nextTick(function() {
-            console.log('[13]:timeout2_nextTick');//13
-        })
-        new Promise(function(resolve) {
-            console.log('[11-1]:timeout2_promise');//11-1
-            resolve();
-        }).then(function() {
-            console.log('[15]:timeout2_then')//15
-        });
-         console.log('[11-2]:timeout2 end');//11-2
-    })
+   
      
     process.nextTick(function() {
         console.log('[5]:glob2_nextTick');//5
@@ -73,4 +74,23 @@
         }).then(function() {
             console.log(+new Date,'[23]:immediate2_then')//23
         })
+    })
+
+     setTimeout(function() {
+        console.log('[10]:timeout2',+new Date);//10
+        process.nextTick(function() {
+            console.log('[13]:timeout2_nextTick');//13
+        })
+        new Promise(function(resolve) {
+            console.log('[11-1]:timeout2_promise');//11-1
+            resolve();
+        }).then(function() {
+            console.log('[15-1]:timeout2_then',+new Date)//15-1
+        })
+        //.then(function() {
+        //     console.warn('[15-2]:timeout2_then2',+new Date)//15-2
+        // }).then(function() {
+        //     console.warn('[15-3]:timeout2_then3',+new Date)//15-3
+        // });
+         console.log('[11-2]:timeout2 end!!!!!!!!!');//11-2
     })
