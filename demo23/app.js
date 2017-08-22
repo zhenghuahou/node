1f68599e36574f3856709618c72652957895f0a5
houzhenghua <houzhenghua@jd.com>
2017-08-22 16:53:09 +0800

    add

---
+++ demo23/app.js
@@ -0,0 +1,12 @@
+ // NODE_ENV=test xxaa=23  DEBUG=12 debug=true  node demo23/app.js
+ /*
+ process.env.NODE_ENV:::: test
+ process.env.xxaa:: 23
+ process.env.debug:: true
+ process.env.DEBUG:: 12
+ */
+ console.log(' process.env::::',process.env);
+ console.log(' process.env.NODE_ENV::::',process.env.NODE_ENV);
+ console.log(' process.env.xxaa::',process.env.xxaa);
+ console.log(' process.env.debug::',process.env.debug);
+ console.log(' process.env.DEBUG::',process.env.DEBUG);
